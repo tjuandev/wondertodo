5979484b26167befd3fa99796a3398db02bd739f
tjuandev <tpydeveloper@gmail.com>
2022-06-26 13:48:47 -0300

    feat: Adding icons assets

---
+++ src/assets/icons/Arrow.tsx
@@ -0,0 +1,21 @@
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      fill="none"
+      viewBox="0 0 18 18"
+    >
+      <path
+        stroke="#EEE"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M1 9h16M9 1l8 8-8 8"
+      ></path>
+    </svg>
+  )
+}
+
+export default Icon
