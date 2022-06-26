35916b2a4190a90d1975eb4a65b4b05ddebb0e60
tjuandev <tpydeveloper@gmail.com>
2022-06-26 13:19:29 -0300

    feat: Re-adding Styled components SSR

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,4 +1,12 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document'
+import { Fragment } from 'react'
+import { ServerStyleSheet } from 'styled-components'
 export default class MyDocument extends Document {
   render() {
     return (
@@ -33,5 +41,30 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     )
+  }
+
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [
+          <Fragment key={1}>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </Fragment>
+        ]
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 }
