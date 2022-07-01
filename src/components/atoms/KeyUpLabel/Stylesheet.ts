57dad7483c09ddc5ee35845dbb333ea1631f47e4
tjuandev <tpydeveloper@gmail.com>
2022-07-01 18:47:27 -0300

    feat: improving label spacement

--- src/components/atoms/KeyUpLabel/Stylesheet.ts
+++ src/components/atoms/KeyUpLabel/Stylesheet.ts
@@ -4,7 +4,7 @@ export const LabelWrapper = styled.span`
   background: rgba(0, 0, 0, 0.5);
   border-radius: max(5px);
 
-  padding: 0 0.5rem;
+  padding: 0.25rem 0.5rem;
   font-size: 1rem;
   font-weight: 800;
 `
