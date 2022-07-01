51eb5a9a252a282f9c76c5f4922888b30754f6b1
tjuandev <tpydeveloper@gmail.com>
2022-07-01 19:53:51 -0300

    feat: Improving Todos styling

--- src/components/organisms/Todos/Stylesheet.ts
+++ src/components/organisms/Todos/Stylesheet.ts
@@ -1,12 +1,26 @@
 import styled from 'styled-components'
 
-export const TabsContainer = styled.header``
-
 export const Container = styled.section`
   max-width: 900px;
+
+  display: grid;
+  grid-template-rows: fit-content 1fr fit-content;
+  grid-row-gap: 4rem;
+`
+
+export const TabsContainer = styled.header`
+  display: flex;
+
+  width: 25%;
+
+  > h2:last-child {
+    margin-left: -5px;
+  }
 `
 
 export const ListWrapper = styled.ul`
+  overflow-y: auto;
+
   li {
     list-style: none;
   }
