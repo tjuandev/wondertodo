96e7f3666f5afdd5d3515b61ddaf412b89d43769
tjuandev <tpydeveloper@gmail.com>
2022-07-30 15:20:06 -0300

    feat: Passing a generic type in getAllFromTable function

--- src/infra/http/db/read/index.ts
+++ src/infra/http/db/read/index.ts
@@ -1,7 +1,7 @@
 import { dbClient } from 'infra/adapters'
 
-export const getAllFromTable = async (table: string) => {
-  const response = await dbClient.from(table).select()
+export const getAllFromTable = async <Type>(table: string) => {
+  const response = await dbClient.from<Type>(table).select()
 
   return response
 }
